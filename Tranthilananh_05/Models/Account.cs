b6c0101f18dab052bfcda7302aecf4d5a33ba485
Tranthilananh1803 <tranlananhquyet2318@gmail.com>
2021-06-06 15:49:27 +0700

    comit

---
+++ Tranthilananh_05/Models/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Tranthilananh_05.Models
+{
+    [Table("Account")]
+    public class Account
+    {
+        [Key]
+        [Required(ErrorMessage = "User name is required.")]
+        public string Usename { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
